0db956604886ffb7b23cdf2f50a04fdbbc2fd330
rajatsaxena6 <rajat.saxena6@wipro.com>
2017-03-18 19:57:33 +0530

    inital commit

---
+++ Foodies-master/client/src/js/components/HomeComponent.jsx
@@ -0,0 +1,40 @@
+
+import React from 'react';
+
+import SearchComponent from './SearchComponent.jsx';
+import RestaurantBoxComponent from './RestaurantBoxComponent.jsx';
+
+
+export default class HomeComponent extends React.Component{
+constructor(){
+super();
+this.state={restaurants:[]};
+
+this.setRestaurantAr=this.setRestaurantAr.bind(this);
+
+}
+
+setRestaurantAr(restAr) {
+this.setState({restaurants:restAr});
+
+}
+
+
+
+
+render(){
+var d=this.state.data;
+var a=this.state.articles;
+return(
+<div>
+
+<SearchComponent newsSource={this.setRestaurantAr.bind(this)}/>
+<RestaurantBoxComponent  restAr={this.state.restaurants}/>
+
+
+</div>
+
+)
+}
+
+}
